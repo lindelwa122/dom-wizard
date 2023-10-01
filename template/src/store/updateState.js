6459182c1a7dfc0d9f6120cea461d15d3c6a557d
Dipesh Ranjan <sinhadipesh25@gmail.com>
2023-10-02 02:46:36 +0530

    updateState jsdoc added

--- template/src/store/updateState.js
+++ template/src/store/updateState.js
@@ -1,3 +1,15 @@
+/**
+ * Updates the state of the store.
+ *
+ * @param {string} key The key of the property to update.
+ * @param {*} newValue The new value for the property.
+ * @returns {this} The store object, so that calls can be chained.
+ *
+ * @throws {Error} If the key is not a string.
+ * @throws {Error} If the key does not exist in the store.
+ * @throws {Error} If the new value is undefined or null.
+ */
+
 function updateState(key, newValue) {
     if (!key || typeof key !== 'string') {
       throw new Error(`Key  ${key} must be a string.`);
