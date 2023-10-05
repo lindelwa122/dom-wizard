3ed771780c6df36f6a86e82eb6a146030cdf780e
Lindelwa <lindelwa.codes@gmail.com>
2023-10-05 20:51:23 +0200

    Add _createElement

--- modules/domManager.js
+++ modules/domManager.js
@@ -1,4 +1,42 @@
+import router from './router';
+
 const domManager = () => {
+  const _createElement = (element) => {
+    if (!element.tagName) {
+      throw new Error('tagName is undefined. An element cannot be created without a tagName.');
+    }
+
+    const el = document.createElement(element.tagName);
+
+    if (!el) {
+      throw new Error(`The specified tagName (${element.tagName}) is not recognised. Ensure this value is valid for your elements to be created correctly.`);
+    }
+
+    if (!element.options) return el;
+
+    if (element.options.className) el.className = className;
+
+    if (element.options.classList) {
+      el.classList = options.classList.join(" ");
+    }
+
+    if (element.id) {
+      el.id = id;
+    }
+
+    if (element.link) {
+      router.configureLink(lement.link);
+    }
+
+    if (element.attributes) {
+      for (const [prop, value] of Object.entries(element.attributes)) {
+        el[prop] = value;
+      }
+    }
+
+    return el;
+  }
+
   const _createDOMTree = (element) => {
     if (!element) {
       throw new Error('element is undefined.');
