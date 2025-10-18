e9f14eccaf7c8bca0abdd2ce40ac03e32e4dade0
gumumjhb025 <gumumjhb025@student.wethinkcode.co.za>
2025-10-18 19:38:30 +0200

    completed all rest method tests

--- modules/apiGateway.js
+++ modules/apiGateway.js
@@ -1,56 +1,98 @@
-export let Get=async(url,body,json=false,handlerCallback,errorCallback,finalCallback)=>{
-    fetch(url,
-        {method:"GET",
-            body:body
-        }
-    ).then((response)=>handlerCallback(response))
-    .catch(err=>errorCallback(err))
-    .finally(res=>finalCallback(res))
-}
+export const Get = async (url, handlerCallback, errorCallback, finalCallback, body = null, json = false) => {
+ 
+    const response = await fetch(url,{method:"GET"});
+    let data;
+ 
+    if (response.ok) {
+      data = json ? await response.json() : await response.text();
+     
+      handlerCallback && handlerCallback(data);
+    } else {
+      data = { status: response.status, error: response.statusText };
+      errorCallback && errorCallback(data);
+    }
+    finalCallback && finalCallback(data);
+    return data;
+  
+};
 
-export let Post=async(url,body,headers={ 'Content-Type': 'application/json' },json=false,handlerCallback,errorCallback,finalCallback)=>{
+export let Post=async(url,body,handlerCallback,errorCallback,finalCallback,json=false,headers={ 'Content-Type': 'application/json' })=>{
 
-    fetch(url,
+    let response=await fetch(url,
         {method:"POST",
             headers:headers,
             body:body
-        }
-    ).then((response)=>handlerCallback(json?response.json():response))
-    .catch(err=>errorCallback(err))
-    .finally(res=>finalCallback(res))
+        })
+
+      if (response.ok) {
+      data = json ? await response.json() : await response.text();
+     
+      handlerCallback && handlerCallback(data);
+    } else {
+      data = { status: response.status, error: response.statusText };
+      errorCallback && errorCallback(data);
+    }
+    finalCallback && finalCallback(data);
+    return data;
+  
 }
 
-export let Put=async(url,body,headers={ 'Content-Type': 'application/json' },json=false,handlerCallback,errorCallback,finalCallback)=>{
-    fetch(url,
+export let Put=async(url,body,handlerCallback,errorCallback,finalCallback,json=false,headers={ 'Content-Type': 'application/json' })=>{
+    let response =await fetch(url,
         {method:"PUT",
             headers:headers,
             body:body
         }
-    ).then((response)=>handlerCallback(json?response.json():response))
-    .catch(err=>errorCallback(err))
-    .finally(res=>finalCallback(res))
+    )
+      if (response.ok) {
+      data = json ? await response.json() : await response.text();
+     
+      handlerCallback && handlerCallback(data);
+    } else {
+      data = { status: response.status, error: response.statusText };
+      errorCallback && errorCallback(data);
+    }
+    finalCallback && finalCallback(data);
+    return data;
+  
 
 }
 
-export let Patch=async(url,body,headers={ 'Content-Type': 'application/json' },json=false,handlerCallback,errorCallback,finalCallback)=>{
-    fetch(url,
+export let Patch=async(url,body,handlerCallback,errorCallback,finalCallback,json=false,headers={ 'Content-Type': 'application/json' })=>{
+    let response =await fetch(url,
         {method:"PATCH",
             headers:headers,
             body:body
         }
-    ).then((response)=>handlerCallback(response))
-    .catch(err=>errorCallback(err))
-    .finally(res=>finalCallback(res))
+    )
+      if (response.ok) {
+      data = json ? await response.json() : await response.text();
+     
+      handlerCallback && handlerCallback(data);
+    } else {
+      data = { status: response.status, error: response.statusText };
+      errorCallback && errorCallback(data);
+    }
+    finalCallback && finalCallback(data);
+    return data;
+  
 
 }
 
-export let Delete=async(url,body,json=false,handlerCallback,errorCallback,finalCallback)=>{
-    fetch(url,
-        {method:"DELETE",
-            body:body
-        }
-    ).then((response)=>handlerCallback(response))
-    .catch(err=>errorCallback(err))
-    .finally(res=>finalCallback(res))
+export let Delete=async(url,handlerCallback,errorCallback,finalCallback)=>{
+    let response =await fetch(url,
+        {method:"DELETE"}
+    )
+      if (response.ok) {
+      data =  await response.json() ;
+     
+      handlerCallback && handlerCallback(data);
+    } else {
+      data = { status: response.status, error: response.statusText };
+      errorCallback && errorCallback(data);
+    }
+    finalCallback && finalCallback(data);
+    return data;
+  
 
 }
